518d6ebc5098aec1bc7ac5d87ddc433047f4b53d
Niu Zhihong <zhihong@nzhnb.com>
2025-08-11 14:07:29 +0800

    refactor: slab
    Signed-off-by: Niu Zhihong <zhihong@nzhnb.com>

--- src/include/slab.h
+++ src/include/slab.h
@@ -5,67 +5,35 @@
 #ifndef BMALLOC_SRC_INCLUDE_SLAB_H_
 #define BMALLOC_SRC_INCLUDE_SLAB_H_
 
-#include <cstddef>
-#include <cstdint>
+#include <stdlib.h>
 
 #include "allocator_base.h"
 
-class Slab : public AllocatorBase {
- public:
-  /**
-   * @brief 构造 Slab 分配器
-   * @param name 分配器名称
-   * @param start_addr 管理的内存起始地址
-   * @param bytes 管理的字节数
-   */
-  explicit Slab(const char* name, uint64_t start_addr, size_t bytes);
+typedef struct kmem_cache_s kmem_cache_t;
+#define BLOCK_SIZE (4096)
+#define CACHE_L1_LINE_SIZE (64)
 
-  /// @name 构造/析构函数
-  /// @{
-  Slab() = default;
-  Slab(const Slab&) = delete;
-  Slab(Slab&&) = default;
-  auto operator=(const Slab&) -> Slab& = delete;
-  auto operator=(Slab&&) -> Slab& = default;
-  ~Slab() override = default;
-  /// @}
+void kmem_init(void *space, int block_num);
 
-  /**
-   * @brief 分配指定字节的内存
-   * @param bytes 要分配的字节
-   * @return uint64_t 分配的内存起始地址，失败时返回0
-   */
-  [[nodiscard]] auto Alloc(size_t bytes) -> uint64_t override;
+kmem_cache_t *kmem_cache_create(const char *name, size_t size,
+                                void (*ctor)(void *),
+                                void (*dtor)(void *));  // Allocate cache
 
-  /**
-   * @brief 在指定地址分配指定字节的内存
-   * @param addr 指定的地址
-   * @param bytes 要分配的字节
-   * @return true 分配成功
-   * @return false 分配失败
-   */
-  auto Alloc(uint64_t addr, size_t bytes) -> bool override;
+int kmem_cache_shrink(kmem_cache_t *cachep);  // Shrink cache
 
-  /**
-   * @brief 释放指定地址的内存
-   * @param addr 要释放的内存起始地址
-   * @param bytes 要释放的字节
-   */
-  void Free(uint64_t addr, size_t bytes) override;
+void *kmem_cache_alloc(kmem_cache_t *cachep);  // Allocate one object from cache
 
-  /**
-   * @brief 获取已使用的字节
-   * @return size_t 已使用的字节
-   */
-  [[nodiscard]] auto GetUsedCount() const -> size_t override;
+void kmem_cache_free(kmem_cache_t *cachep,
+                     void *objp);  // Deallocate one object from cache
 
-  /**
-   * @brief 获取空闲的字节
-   * @return size_t 空闲的字节
-   */
-  [[nodiscard]] auto GetFreeCount() const -> size_t override;
+void *kmalloc(size_t size);  // Alloacate one small memory buffer
 
- private:
-};
+void kfree(const void *objp);  // Deallocate one small memory buffer
+
+void kmem_cache_destroy(kmem_cache_t *cachep);  // Deallocate cache
+
+void kmem_cache_info(kmem_cache_t *cachep);  // Print cache info
+
+int kmem_cache_error(kmem_cache_t *cachep);  // Print error message
 
 #endif /* BMALLOC_SRC_INCLUDE_SLAB_H_ */
