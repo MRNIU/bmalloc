ae2010d3190f1e43ec43e2fdb1f62961cad54fbb
Niu Zhihong <zhihong@nzhnb.com>
2025-09-15 10:12:54 +0800

    feat: add log
    Signed-off-by: Niu Zhihong <zhihong@nzhnb.com>

--- src/bmalloc.cpp
+++ src/bmalloc.cpp
@@ -5,20 +5,6 @@
 #include <cstring>
 
 extern "C" {
-/**
- * @brief Weak implementation of memset function that can be overridden
- * @param ptr Pointer to the memory area to be filled
- * @param value Value to be set (will be converted to unsigned char)
- * @param size Number of bytes to be set
- * @return void* Returns the pointer to the memory area ptr
- * @note This function uses weak linkage, allowing users to provide their own
- *       implementation that will override this default one at link time
- * @example To override this function, define your own memset:
- *          extern "C" void* memset(void* ptr, int value, size_t size) {
- *              // Your custom implementation here
- *              return your_optimized_memset(ptr, value, size);
- *          }
- */
 __attribute__((weak)) void* memset(void* ptr, int value, size_t size) {
   // Default implementation - could use inline assembly, SIMD, or other
   // optimizations
