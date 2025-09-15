f5488e89102de08ccc9f023e990a967491f1c570
Niu Zhihong <zhihong@nzhnb.com>
2025-09-15 10:18:12 +0800

    feat: update mem*
    Signed-off-by: Niu Zhihong <zhihong@nzhnb.com>

--- src/bmalloc.cpp
+++ src/bmalloc.cpp
@@ -4,21 +4,16 @@
 
 #include <cstring>
 
+#ifdef __cplusplus
 extern "C" {
-__attribute__((weak)) void* memset(void* ptr, int value, size_t size) {
-  // Default implementation - could use inline assembly, SIMD, or other
-  // optimizations
-  if (!ptr || size == 0) {
-    return ptr;
+#endif
+__attribute__((weak)) void *memset(void *dest, int val, size_t n) {
+  unsigned char *ptr = dest;
+  while (n-- > 0) {
+    *ptr++ = val;
   }
-
-  unsigned char* byte_ptr = static_cast<unsigned char*>(ptr);
-  unsigned char byte_value = static_cast<unsigned char>(value);
-
-  for (size_t i = 0; i < size; ++i) {
-    byte_ptr[i] = byte_value;
-  }
-
-  return ptr;
+  return dest;
 }
+#ifdef __cplusplus
 }
+#endif
